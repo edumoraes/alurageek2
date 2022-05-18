d9fb3b5e44e73988f389d24b405b25a22c8997a1
Edu Moraes <edumoraesdg@gmail.com>
2022-05-17 23:47:37 -0400

    Grid produtos

--- src/components/gridprodutos/GridProdutos.jsx
+++ src/components/gridprodutos/GridProdutos.jsx
@@ -13,7 +13,7 @@ export default function GridProdutos(props) {
       <div className={styles.containerGrid}>
 
         <div className={styles.tituloSecao}>
-          <h2 className='titulo-h2'>Nome da seção</h2>
+          <h2 className='titulo-h2'>Star Wars</h2>
           <Link href="#">
             <a className='link'>Ver tudo<BsArrowRight /></a>
           </Link>
@@ -22,44 +22,44 @@ export default function GridProdutos(props) {
         <div className={styles.produtos}>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/1-star-wars.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
-            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <h3>R$ 32,00</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
           </div>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/2-star-wars.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
-            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <h3>R$ 32,00</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
           </div>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/3-star-wars.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
-            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <h3>R$ 32,00</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
           </div>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/4-star-wars.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
-            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <h3>R$ 32,00</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
           </div>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/5-star-wars.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
-            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <h3>R$ 32,00</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
           </div>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/6-star-wars.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
-            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <h3>R$ 32,00</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
           </div>
 
@@ -70,7 +70,7 @@ export default function GridProdutos(props) {
       <div className={styles.containerGrid}>
 
         <div className={styles.tituloSecao}>
-          <h2 className='titulo-h2'>Nome da seção</h2>
+          <h2 className='titulo-h2'>Consoles</h2>
           <Link href="#">
             <a className='link'>Ver tudo<BsArrowRight /></a>
           </Link>
@@ -79,42 +79,99 @@ export default function GridProdutos(props) {
         <div className={styles.produtos}>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/1-consoles.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
             <h3>Preço do produto</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
           </div>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/2-consoles.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
             <h3>Preço do produto</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
           </div>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/3-consoles.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
             <h3>Preço do produto</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
           </div>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/4-consoles.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
             <h3>Preço do produto</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
           </div>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/5-consoles.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
             <h3>Preço do produto</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
           </div>
 
           <div className={styles.cardProduto}>
-            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <img src="/img/6-consoles.jpg" className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+          
+        </div>
+
+      </div>
+
+      <div className={styles.containerGrid}>
+
+        <div className={styles.tituloSecao}>
+          <h2 className='titulo-h2'>Diversos</h2>
+          <Link href="#">
+            <a className='link'>Ver tudo<BsArrowRight /></a>
+          </Link>
+        </div>
+
+        <div className={styles.produtos}>
+
+          <div className={styles.cardProduto}>
+            <img src="/img/1-diversos.png" className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src="/img/2-diversos.png" className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src="/img/3-diversos.png" className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src="/img/4-diversos.png" className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src="/img/5-diversos.png" className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src="/img/6-diversos.png" className={styles.imageProduto} /> {/*Adiconar parametros*/}
             <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
             <h3>Preço do produto</h3> {/*Adiconar parametros*/}
             <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
