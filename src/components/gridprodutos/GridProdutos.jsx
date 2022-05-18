43d8c5e7a104336a76884d1f9688d8880a1cca5e
Edu Moraes <edumoraesdg@gmail.com>
2022-05-17 22:54:12 -0400

    Commit aleatória

--- src/components/gridprodutos/GridProdutos.jsx
+++ src/components/gridprodutos/GridProdutos.jsx
@@ -6,15 +6,128 @@ import styles from './GridProdutos.module.css'
 export default function GridProdutos(props) {
   
   return (
-    <div className="container">
-      <h2>Nome da seção</h2>
-      <button>Ver tudo<BsArrowRight /></button>
-      <div className="card">
-        <img src={props.src} className={styles.cardImage} /> {/*Adiconar parametros*/}
-        <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
-        <h3>Preço do produto</h3> {/*Adiconar parametros*/}
-        <Link href="#"><a>Ver produto</a></Link> {/*Adiconar parametros*/}
+    <>
+    
+    <div className='container'>
+      
+      <div className={styles.containerGrid}>
+
+        <div className={styles.tituloSecao}>
+          <h2 className='titulo-h2'>Nome da seção</h2>
+          <Link href="#">
+            <a className='link'>Ver tudo<BsArrowRight /></a>
+          </Link>
+        </div>
+
+        <div className={styles.produtos}>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+        </div>
+
       </div>
+
+      <div className={styles.containerGrid}>
+
+        <div className={styles.tituloSecao}>
+          <h2 className='titulo-h2'>Nome da seção</h2>
+          <Link href="#">
+            <a className='link'>Ver tudo<BsArrowRight /></a>
+          </Link>
+        </div>
+
+        <div className={styles.produtos}>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+
+          <div className={styles.cardProduto}>
+            <img src={props.src} className={styles.imageProduto} /> {/*Adiconar parametros*/}
+            <h4>Nome do Produto</h4> {/*Adiconar parametros*/}
+            <h3>Preço do produto</h3> {/*Adiconar parametros*/}
+            <Link href="#"><a className='link'>Ver produto</a></Link> {/*Adiconar parametros*/}
+          </div>
+          
+        </div>
+
+      </div>
+
+      
+
     </div>
+    
+    </>
   ) 
 }
